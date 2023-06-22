269fac8a1b7d6da5a89eb80b81a8cde58fc457ad
Simran Mandal <simranmandal0211@gmail.com>
2023-06-22 23:34:43 +0530

    display fetched Img

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ $.get("https://dog.ceo/api/breeds/list/all", function(data, status){
     }
     // console.log("dogbreeds taken");
 });
+
+dropDown.change(function(){
+    allowSubmit = true;
+    
+    console.log("dropdown changed");
+});
+
+$('form button').click(function(e){
+    e.preventDefault();
+
+    if(allowSubmit){
+        breed = dropDown.val();
+        displayDog(breed);
+        allowSubmit = false;
+    }
+     console.log("get image clicked");
+});
+
+function displayDog(breed){
+    let url = "https://dog.ceo/api/breed/" + breed + "/images/random";
+
+    $('#breed-img-box img').remove();
+
+    $.get(url, function(data, status){
+        let imgUrl = data.message;
+        breedImage.append('<img src="' + imgUrl + '" alt="' + breed + '">');
+    })
+}
